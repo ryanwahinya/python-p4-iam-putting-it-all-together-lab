7b3810674c2aad94d6eafeed7b6a9ac54b344418
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-09-22 14:08:37 +0300

    fixed errors

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import randint, choice as rc
-
 from faker import Faker
 
 from app import app
@@ -10,54 +9,48 @@ from models import db, Recipe, User
 fake = Faker()
 
 with app.app_context():
-
     print("Deleting all records...")
     Recipe.query.delete()
     User.query.delete()
-
-    fake = Faker()
+    db.session.commit()
 
     print("Creating users...")
 
-    # make sure users have unique usernames
     users = []
-    usernames = []
+    usernames = set()
 
-    for i in range(20):
-        
+    for _ in range(20):
         username = fake.first_name()
         while username in usernames:
             username = fake.first_name()
-        usernames.append(username)
+        usernames.add(username)
 
         user = User(
             username=username,
             bio=fake.paragraph(nb_sentences=3),
-            image_url=fake.url(),
+            image_url=fake.url()
         )
-
-        user.password_hash = user.username + 'password'
-
+        # Set password hash properly:
+        user.password_hash = username + 'password'
         users.append(user)
 
     db.session.add_all(users)
+    db.session.commit()
 
     print("Creating recipes...")
     recipes = []
-    for i in range(100):
+    for _ in range(100):
         instructions = fake.paragraph(nb_sentences=8)
-        
+
         recipe = Recipe(
             title=fake.sentence(),
             instructions=instructions,
-            minutes_to_complete=randint(15,90),
+            minutes_to_complete=randint(15, 90),
+            user=rc(users)
         )
-
-        recipe.user = rc(users)
-
         recipes.append(recipe)
 
     db.session.add_all(recipes)
-    
     db.session.commit()
+
     print("Complete.")
